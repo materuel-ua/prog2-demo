c5764e5b9deb858a5f374b90e4f064de90b4d79e
Miguel A. Teruel <miguelangel.teruel@ua.es>
2025-02-24 11:34:36 +0100

    Creación de test.py

--- Estudiante.py
+++ Estudiante.py
@@ -46,21 +46,3 @@ class Estudiante:
         return edad >= 18
 
 
-# Ejemplo de uso
-if __name__ == "__main__":
-    estudiante1 = Estudiante("Carlos", 20, ["Matemáticas", "Física"])
-    if estudiante1.inscribir_curso("Programación"):
-        print("Curso añadido exitosamente.")
-    else:
-        print("El curso ya estaba inscrito.")
-
-    estudiante1.mostrar_informacion()
-    print(estudiante1)
-    print(repr(estudiante1))
-
-    try:
-        estudiante2 = Estudiante.desde_tupla(("Ana", 17, ["Historia"]))
-        estudiante2.mostrar_informacion()
-        print(Estudiante.es_mayor_de_edad(estudiante2.edad))  # False
-    except ValueError as e:
-        print(f"Error al crear estudiante desde tupla: {e}")
--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-print('hello world')
+from Estudiante import Estudiante
 
-print('hello world 2 ')
+# Ejemplo de uso
+if __name__ == "__main__":
+    estudiante1 = Estudiante("Carlos", 20, ["Matemáticas", "Física"])
+    if estudiante1.inscribir_curso("Programación"):
+        print("Curso añadido exitosamente.")
+    else:
+        print("El curso ya estaba inscrito.")
 
+    estudiante1.mostrar_informacion()
+    print(estudiante1)
+    print(repr(estudiante1))
 
-print('hello world 3')
-
-
-print('hello world 4')
+    try:
+        estudiante2 = Estudiante.desde_tupla(("Ana", 17, ["Historia"]))
+        estudiante2.mostrar_informacion()
+        print(Estudiante.es_mayor_de_edad(estudiante2.edad))  # False
+    except ValueError as e:
+        print(f"Error al crear estudiante desde tupla: {e}")
